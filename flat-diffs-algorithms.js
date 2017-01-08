f60776d7659bb62076bba40138913d3e02ac52e7
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-07 20:35:02 -0600

    algorithm seems to work; now need to test it

---
+++ flat-diffs-algorithms.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const _ = require('lodash');
+
+const Constants = require('./constants.json');
+
+module.exports = {
+  merge: function (flatDiffs) {
+    return _(flatDiffs.slice(0))
+      .groupBy((d) => d.date.value)
+      .map((diffs, time) => {
+        return diffs.reduce((a, d) => {
+          a.forEach((ad) => {
+            if (ad.merge(d)) { return a; }
+          });
+          a.push(d);
+          return a;
+        }, new Array());
+      })
+      .value();
+  },
+
+  scaleDates: function (flatDiffs) {
+    flatDiffs = flatDiffs.slice(0);
+
+    const minDate = flatDiffs.reduce(
+      (a, d) => a > d.date.value ? d.date.value : a, Number.MAX_SAFE_INTEGER);
+
+    flatDiffs
+      .forEach((d) => d.date
+               .translate(-minDate)
+               .scale(1.0/Constants.msPerDay)
+               .floor());
+    return flatDiffs;
+  }
+};
