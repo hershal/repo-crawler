4748a44cc1d9482cf4f4c185afec0208519380a3
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-09 01:14:57 -0600

    svg-renderer: add newline :)

--- svg-renderer.js
+++ svg-renderer.js
@@ -6,7 +6,7 @@ const util = require('util');
 function render(width, height, objs) {
   let str = `<svg viewBox="0 0 ${width} ${height}">\n`;
   str += objs.map((o) => `  <circle cx="${o.x}" cy="${o.y}" r="5"/>`).join('\n');
-  str += '\n</svg>';
+  str += '\n</svg>\n';
   return str;
 }
 
